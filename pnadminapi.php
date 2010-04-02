c5ac25324430e2d3dd93492ed1c98303fee2b63a
yokav <yokav@ed2fc0b8-7847-43f0-bdc3-2354aa5163f2>
2010-04-02 23:47:55 +0000

    Some little modifications

--- pnadminapi.php
+++ pnadminapi.php
@@ -6,8 +6,6 @@
  * @link http://www.zikula.org
  * @version $Id$
  * @license GNU/GPL - http://www.gnu.org/copyleft/gpl.html
- * @package Zikula_Value_Addons
- * @subpackage Ratings
  */
 
 /**
--- pnversion.php
+++ pnversion.php
@@ -6,22 +6,23 @@
  * @link http://www.zikula.org
  * @version $Id$
  * @license GNU/GPL - http://www.gnu.org/copyleft/gpl.html
- * @package Zikula_Value_Addons
- * @subpackage Ratings
  */
 
 $dom = ZLanguage::getModuleDomain('Ratings');
 $modversion['name']           = 'Ratings';
 $modversion['displayname']    = __('Ratings', $dom);
 $modversion['description']    = __('Rate Zikula items.', $dom);
-$modversion['url'] = __('ratings', $dom);
-$modversion['version'] = '2.1';
+$modversion['url'] = __(/*!module name that appears in URL*/'ratings', $dom);
+$modversion['version'] = '2.2';
+
 $modversion['credits'] = 'pndocs/credits.txt';
 $modversion['help'] = 'pndocs/help.txt';
 $modversion['changelog'] = 'pndocs/changelog.txt';
 $modversion['license'] = 'pndocs/license.txt';
-$modversion['coding'] = 'pndocs/coding.txt';
-$modversion['official'] = 1;
+
+$modversion['official'] = true;
+
 $modversion['author'] = 'Jim McDonald';
 $modversion['contact'] = 'http://www.mcdee.net/';
+
 $modversion['securityschema'] = array('Ratings::' => 'Module name:Rating type:Item ID');
