35701d0629449560fc8fa0e5f0bd4ed994b97c5b
yokav <yokav@ed2fc0b8-7847-43f0-bdc3-2354aa5163f2>
2010-04-20 20:43:00 +0000

    Update pnversion.php

--- pnversion.php
+++ pnversion.php
@@ -1,28 +1,28 @@
 <?php
 /**
- * Zikula Application Framework
+ * Ratings
  *
  * @copyright (c) 2002, Zikula Development Team
- * @link http://www.zikula.org
- * @version $Id$
- * @license GNU/GPL - http://www.gnu.org/copyleft/gpl.html
+ * @link      http://code.zikula.org/ratings/
+ * @version   $Id$
+ * @license   GNU/GPL - http://www.gnu.org/copyleft/gpl.html
  */
 
 $dom = ZLanguage::getModuleDomain('Ratings');
 $modversion['name']           = 'Ratings';
 $modversion['displayname']    = __('Ratings', $dom);
-$modversion['description']    = __('Rate Zikula items.', $dom);
-$modversion['url'] = __(/*!module name that appears in URL*/'ratings', $dom);
-$modversion['version'] = '2.2';
+$modversion['description']    = __('The Ratings module provides a hook to other modules, allowing your users to rate a module item.', $dom);
+$modversion['url']            = __(/*!module name that appears in URL*/'ratings', $dom);
+$modversion['version']        = '2.3';
 
-$modversion['credits'] = 'pndocs/credits.txt';
-$modversion['help'] = 'pndocs/help.txt';
-$modversion['changelog'] = 'pndocs/changelog.txt';
-$modversion['license'] = 'pndocs/license.txt';
+$modversion['credits']        = 'pndocs/credits.txt';
+$modversion['help']           = 'pndocs/help.txt';
+$modversion['changelog']      = 'pndocs/changelog.txt';
+$modversion['license']        = 'pndocs/license.txt';
 
-$modversion['official'] = true;
+$modversion['official']       = false;
 
-$modversion['author'] = 'Jim McDonald';
-$modversion['contact'] = 'http://www.mcdee.net/';
+$modversion['author']         = 'Jim McDonald, Christophe Beaujean';
+$modversion['contact']        = 'http://code.zikula.org/ratings';
 
 $modversion['securityschema'] = array('Ratings::' => 'Module name:Rating type:Item ID');
