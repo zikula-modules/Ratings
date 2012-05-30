96f3e0ca59b853036a467233caf2dd76f790a9b1
Guite <info@guite.de>
2012-05-30 20:49:39 +0200

    fixed ajax controller

--- lib/Ratings/Controller/Ajax.php
+++ lib/Ratings/Controller/Ajax.php
@@ -8,40 +8,42 @@
  * @license GNU/GPL - http://www.gnu.org/copyleft/gpl.html
 */
 
-/**
- * Log a vote and display the results form
- *
- * @author Mark West
- * @param pollid the poll to vote on
- * @param voteid the option to vote on
- * @return string updated display for the block
- */
-function ratings_ajax_rate()
+class Ratings_Controller_Ajax extends Zikula_Controller_AbstractAjax
 {
-    $modname =    DataUtil::convertFromUTF8(FormUtil::getPassedValue('modname', null, 'POST'));
-    $objectid =   DataUtil::convertFromUTF8(FormUtil::getPassedValue('objectid', null, 'POST'));
-    $rating =     DataUtil::convertFromUTF8(FormUtil::getPassedValue('rating', null, 'POST'));
-    $ratingtype = DataUtil::convertFromUTF8(FormUtil::getPassedValue('ratingtype', ModUtil::getVar('Ratings', 'defaultstyle'), 'POST'));
-    $returnurl =  DataUtil::convertFromUTF8(FormUtil::getPassedValue('returnurl', null, 'POST'));
+    /**
+     * Log a vote and display the results form
+     *
+     * @author Mark West
+     * @param pollid the poll to vote on
+     * @param voteid the option to vote on
+     * @return string updated display for the block
+     */
+    public function rate()
+    {
+        $modName    = $this->request->request->get('modname', null);
+        $objectid   = $this->request->request->get('objectid', null);
+        $rating     = $this->request->request->get('rating', null);
+        $ratingType = $this->request->request->get('ratingtype', $this->getVar('defaultstyle'));
+        $returnUrl  = $this->request->request->get('returnurl', null);
 
-    if (!SecurityUtil::checkPermission('Ratings::', "$modname:$ratingtype:$objectid", ACCESS_COMMENT)) {
-        AjaxUtil::error($this->__('Sorry! No authorization to access this module.'));
+        $instance = $modName . ':' . $ratingType . ':' . $objectid;
+        $this->throwForbiddenUnless(SecurityUtil::checkPermission($this->name . '::', $instance, ACCESS_COMMENT));
+
+        // log rating of item
+        $newRating = ModUtil::apiFunc($this->name, 'user', 'rate',
+                                array('modname'    => $modName,
+                                      'objectid'   => $objectid,
+                                      'ratingtype' => $ratingType,
+                                      'rating'     => $rating));
+
+        // get the new output
+        $result = ModUtil::func($this->name, 'user', 'display',
+                                array('objectid'  => $objectid,
+                                      'extrainfo' => array('module'     => $modName,
+                                                           'returnurl'  => $returnUrl)));
+
+
+        // return the new content for the block
+        return array('result' => $result);
     }
-
-    // log rating of item
-    $newrating = ModUtil::apiFunc('Ratings', 'user', 'rate',
-                              array('modname'    => $modname,
-                                    'objectid'   => $objectid,
-                                    'ratingtype' => $ratingtype,
-                                    'rating'     => $rating));
-
-    // get the new output
-    $result = ModUtil::func('Ratings', 'user', 'display',
-                        array('objectid'  => $objectid,
-                              'extrainfo' => array('module'     => $modname,
-                                                   'returnurl'  => $returnurl)));
-
-
-    // return the new content for the block
-    return array('result' => $result);
 }
